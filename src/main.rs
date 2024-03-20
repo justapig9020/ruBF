9d26816d30a9e3642e2124b4f8491ecbf4e410a4
justapig9020 <justapig9020@gmail.com>
2024-03-20 16:11:20 +0800

    Allow cli to set input / output file

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,38 @@ mod tap;
 use anyhow::Result;
 use clap::Parser;
 use std::fs::read_to_string;
-use std::io;
+use std::fs::File;
+use std::io::{self, Read, Write};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
     program: String,
+    #[arg(short, long)]
+    input: Option<String>,
+    #[arg(short, long)]
+    output: Option<String>,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let mut stdin = io::stdin();
-    let mut stdout = io::stdout();
+    let mut input: Box<dyn Read> = if let Some(input_name) = args.input {
+        let input_file = File::open(input_name)?;
+        Box::new(input_file)
+    } else {
+        let stdin = io::stdin();
+        Box::new(stdin)
+    };
+    let mut output: Box<dyn Write> = if let Some(output_name) = args.output {
+        let output_file = File::create(output_name)?;
+        Box::new(output_file)
+    } else {
+        let stdout = io::stdout();
+        Box::new(stdout)
+    };
     let program = read_to_string(args.program)?;
-    let mut vm = brainfuck::VirtualMachine::new(&mut stdin, &mut stdout);
+    let mut vm = brainfuck::VirtualMachine::new(&mut input, &mut output);
     vm.run(&program)?;
     Ok(())
 }
